8e4490785c0229812f689c624b40e639430e7889
Beerkens,Ruud R.J.M <r.beerkens@student.fontys.nl>
2016-06-21 13:20:39 +0200

    javadoc iRekeningmuteerder

--- src/bank/bankieren/IRekeningMuteerder.java
+++ src/bank/bankieren/IRekeningMuteerder.java
@@ -13,8 +13,18 @@ import java.rmi.RemoteException;
  * @author Joel
  */
 public interface IRekeningMuteerder extends Remote{
-	
+	/**
+         * Deze methode roept de muteer functie aan bij het opgegeven rekeningnummer aan en geeft hier een bedrag amount aan mee
+         * @param rekeningNummer het rekeningnummer waarvan het geld gemuteerd moet worden
+         * @param amount het bedrag dat overgeschreven moet worden
+         * @return true als het geld overgeschreven is false als het rekeningnummer niet bekend is
+         * @throws RemoteException 
+         */
 	boolean muteer(int rekeningNummer,Money amount) throws RemoteException;
-	
+	/**
+         * geeft de banknaam terug.
+         * @return de bank naam
+         * @throws RemoteException 
+         */
 	String getName() throws RemoteException;
 }
